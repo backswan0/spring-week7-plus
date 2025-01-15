d155509a6eb9b75a5152262efa1febdcafd85b78
backswan0 <rnrwprh3434@gmail.com>
2025-01-15 15:20:08 +0900

    :test_tube: test/jh-main-level 1-4: controller test (todo)
    - fix HTTP status code from OK to Bad Request to ensure proper test execution
    
    - create shared data to avoid unnecessary input values in tests

---
+++ src/test/java/org/example/expert/SharedData.java
@@ -0,0 +1,58 @@
+package org.example.expert;
+
+import java.time.LocalDateTime;
+import org.example.expert.domain.common.dto.AuthUserDto;
+import org.example.expert.domain.todo.entity.Todo;
+import org.example.expert.domain.user.dto.response.UserResponseDto;
+import org.example.expert.domain.user.entity.User;
+import org.example.expert.domain.user.enums.UserRole;
+import org.springframework.test.util.ReflectionTestUtils;
+
+public class SharedData {
+
+    public static final AuthUserDto AUTH_USER_DTO = new AuthUserDto(
+        1L,
+        "user1@test.com",
+        UserRole.USER,
+        "사용자1"
+    );
+    public static final User USER = User.fromAuthUser(AUTH_USER_DTO);
+    public static final UserResponseDto USER_RESPONSE_DTO = new UserResponseDto(USER);
+    public static final Todo TODO = new Todo(
+        "일정 제목",
+        "일정 내용",
+        "날씨",
+        USER
+    );
+
+    static {
+        ReflectionTestUtils.setField(
+            TODO,
+            "id",
+            1L
+        );
+
+        ReflectionTestUtils.setField(
+            TODO,
+            "createdAt",
+            LocalDateTime.of(
+                2025,
+                1,
+                15,
+                10,
+                0
+            )
+        );
+
+        ReflectionTestUtils.setField(
+            TODO,
+            "modifiedAt",
+            LocalDateTime.of(2025,
+                1,
+                15,
+                10,
+                0
+            )
+        );
+    }
+}
