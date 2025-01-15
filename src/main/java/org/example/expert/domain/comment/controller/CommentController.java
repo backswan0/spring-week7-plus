b9dfb7ccd1cdd5be6ff66b3d3968c58635dc6396
backswan0 <rnrwprh3434@gmail.com>
2025-01-15 09:55:04 +0900

    :bug: fix/jh-main-level 1-1: comment (controller & service)
    - correct the position of 'Transactional(readOnly = true)' annotation to prevent ServletException
    
    - rename variables and methods for improved readability and clarity
    
    - break lines to improve readability when handling multiple parameters
    
    - refine HTTP status codes in controller with ResponseEntity
    
    - replace enhanced for-loop with stream and map for simplicity

--- src/main/java/org/example/expert/domain/comment/controller/CommentController.java
+++ src/main/java/org/example/expert/domain/comment/controller/CommentController.java
@@ -1,17 +1,22 @@
 package org.example.expert.domain.comment.controller;
 
 import jakarta.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.example.expert.domain.comment.dto.request.CommentSaveRequest;
-import org.example.expert.domain.comment.dto.response.CommentResponse;
-import org.example.expert.domain.comment.dto.response.CommentSaveResponse;
+import org.example.expert.domain.comment.dto.request.CommentSaveRequestDto;
+import org.example.expert.domain.comment.dto.response.CommentResponseDto;
+import org.example.expert.domain.comment.dto.response.CommentSaveResponseDto;
 import org.example.expert.domain.comment.service.CommentService;
 import org.example.expert.domain.common.annotation.Auth;
-import org.example.expert.domain.common.dto.AuthUser;
+import org.example.expert.domain.common.dto.AuthUserDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,16 +25,28 @@ public class CommentController {
     private final CommentService commentService;
 
     @PostMapping("/todos/{todoId}/comments")
-    public ResponseEntity<CommentSaveResponse> saveComment(
-            @Auth AuthUser authUser,
-            @PathVariable long todoId,
-            @Valid @RequestBody CommentSaveRequest commentSaveRequest
+    public ResponseEntity<CommentSaveResponseDto> saveComment(
+        @Auth AuthUserDto authUser,
+        @PathVariable("todoId") long todoId,
+        @Valid @RequestBody CommentSaveRequestDto requestDto
     ) {
-        return ResponseEntity.ok(commentService.saveComment(authUser, todoId, commentSaveRequest));
+        CommentSaveResponseDto responseDto = commentService.saveComment(
+            authUser,
+            todoId,
+            requestDto
+        );
+
+        return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
 
     @GetMapping("/todos/{todoId}/comments")
-    public ResponseEntity<List<CommentResponse>> getComments(@PathVariable long todoId) {
-        return ResponseEntity.ok(commentService.getComments(todoId));
+    public ResponseEntity<List<CommentResponseDto>> getAllComments(
+        @PathVariable("todoId") long todoId
+    ) {
+        List<CommentResponseDto> responseDtoList = new ArrayList<>();
+
+        responseDtoList = commentService.getComments(todoId);
+
+        return new ResponseEntity<>(responseDtoList, HttpStatus.OK);
     }
 }
--- src/main/java/org/example/expert/domain/comment/service/CommentService.java
+++ src/main/java/org/example/expert/domain/comment/service/CommentService.java
@@ -1,65 +1,65 @@
 package org.example.expert.domain.comment.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.example.expert.domain.comment.dto.request.CommentSaveRequest;
-import org.example.expert.domain.comment.dto.response.CommentResponse;
-import org.example.expert.domain.comment.dto.response.CommentSaveResponse;
+import org.example.expert.domain.comment.dto.request.CommentSaveRequestDto;
+import org.example.expert.domain.comment.dto.response.CommentResponseDto;
+import org.example.expert.domain.comment.dto.response.CommentSaveResponseDto;
 import org.example.expert.domain.comment.entity.Comment;
 import org.example.expert.domain.comment.repository.CommentRepository;
-import org.example.expert.domain.common.dto.AuthUser;
+import org.example.expert.domain.common.dto.AuthUserDto;
 import org.example.expert.domain.common.exception.InvalidRequestException;
 import org.example.expert.domain.todo.entity.Todo;
 import org.example.expert.domain.todo.repository.TodoRepository;
-import org.example.expert.domain.user.dto.response.UserResponse;
+import org.example.expert.domain.user.dto.response.UserResponseDto;
 import org.example.expert.domain.user.entity.User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
 public class CommentService {
 
     private final TodoRepository todoRepository;
     private final CommentRepository commentRepository;
 
     @Transactional
-    public CommentSaveResponse saveComment(AuthUser authUser, long todoId, CommentSaveRequest commentSaveRequest) {
+    public CommentSaveResponseDto saveComment(AuthUserDto authUser, long todoId,
+        CommentSaveRequestDto commentSaveRequestDto) {
         User user = User.fromAuthUser(authUser);
         Todo todo = todoRepository.findById(todoId).orElseThrow(() ->
-                new InvalidRequestException("Todo not found"));
+            new InvalidRequestException("Todo not found"));
 
         Comment newComment = new Comment(
-                commentSaveRequest.getContents(),
-                user,
-                todo
+            commentSaveRequestDto.getContents(),
+            user,
+            todo
         );
 
         Comment savedComment = commentRepository.save(newComment);
 
-        return new CommentSaveResponse(
-                savedComment.getId(),
-                savedComment.getContents(),
-                new UserResponse(user.getId(), user.getEmail())
+        return new CommentSaveResponseDto(
+            savedComment,
+            new UserResponseDto(user)
         );
     }
 
-    public List<CommentResponse> getComments(long todoId) {
-        List<Comment> commentList = commentRepository.findByTodoIdWithUser(todoId);
+    @Transactional(readOnly = true)
+    public List<CommentResponseDto> getComments(long todoId) {
+        List<Comment> commentList = new ArrayList<>();
 
-        List<CommentResponse> dtoList = new ArrayList<>();
-        for (Comment comment : commentList) {
-            User user = comment.getUser();
-            CommentResponse dto = new CommentResponse(
-                    comment.getId(),
-                    comment.getContents(),
-                    new UserResponse(user.getId(), user.getEmail())
-            );
-            dtoList.add(dto);
-        }
+        commentList = commentRepository.findByTodoIdWithUser(todoId);
+
+        List<CommentResponseDto> dtoList = new ArrayList<>();
+
+        dtoList = commentList.stream()
+            .map(comment -> new CommentResponseDto(
+                    comment,
+                    new UserResponseDto(comment.getUser())
+                )
+            ).toList();
+
         return dtoList;
     }
 }
