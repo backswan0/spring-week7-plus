0b5532753d2356597516879305af36338c7968c4
backswan0 <rnrwprh3434@gmail.com>
2025-01-15 15:44:52 +0900

    :memo: chore/jh-main-entity & controller & service (todo, user)
    - rename variables and methods for improved readability and clarity

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -1,5 +1,6 @@
 package org.example.expert.domain.todo.repository;
 
+import java.util.Optional;
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,15 +8,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.Optional;
-
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
+    @Query("SELECT t FROM Todo t "
+        + "LEFT JOIN FETCH t.user u "
+        + "ORDER BY t.modifiedAt DESC"
+    )
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
     @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN t.user " +
-            "WHERE t.id = :todoId")
+        "LEFT JOIN t.user " +
+        "WHERE t.id = :todoId")
     Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 }
