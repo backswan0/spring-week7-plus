5489d4a5150a43527fe3e1cdf0fbd010df24bbe7
backswan0 <rnrwprh3434@gmail.com>
2025-01-16 17:26:36 +0900

    :bug: fix/jh-main-level 2-7: N+1 problem resolving
    - utilize entity graph annotation to optimize query fetching
    - apply fetch join to prevent N+1 query issue

--- src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
@@ -1,14 +1,19 @@
 package org.example.expert.domain.comment.repository;
 
+import java.util.List;
 import org.example.expert.common.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
-
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    @Query("SELECT c FROM Comment c JOIN c.user WHERE c.todo.id = :todoId")
-    List<Comment> findByTodoIdWithUser(@Param("todoId") Long todoId);
+    @Query(
+        "SELECT c FROM Comment c "
+            + "JOIN FETCH c.user "
+            + "WHERE c.todo.id = :todoId"
+    )
+    List<Comment> findByTodoIdWithUser(
+        @Param("todoId") Long todoId
+    );
 }
